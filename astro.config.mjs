ad59ab236f5bd7b55a2b7d111116bbb6712449a4
Robin Kennedy-Reid <robin.kennedy-reid@stfc.ac.uk>
2022-06-29 22:47:18 +0100

    🚧 Try setting new site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: "https://carneddau.github.io",
-  base: "website",
+  site: "https://robin.carneddau.cymru",
 });
